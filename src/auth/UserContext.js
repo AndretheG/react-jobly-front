36648d05258bb21fcb59a8b551d0a1601e20914d
andre <dr.dre212@gmail.com>
2023-11-17 01:41:31 -0500

    Added all files

---
+++ src/auth/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// COntext: provides currentUser object and setter for it throughout app
+
+const UserContext = React.createContext();
+
+export default UserContext;
---
+++ src/common/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LoadingSpinner.css";
+
+// Loading message used by components that feth API data
+
+function LoadingSpinner() {
+    return (
+        <div className="LoadingSpinner">
+            Loading ...
+        </div>
+    );
+}
+
+export default LoadingSpinner;
